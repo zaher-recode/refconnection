dbdef8c05d044ac23a984dc8c927f54204bf0214
zaher <recode.zaher@gmail.com>
2019-08-20 15:48:35 +0800

    db ref

--- db/migrate/20190820033520_devise_create_users.rb
+++ db/migrate/20190820033520_devise_create_users.rb
@@ -9,6 +9,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :phone 
       t.string :address
       t.boolean :volunteer, default: false
+      t.boolean :refugee, default: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
