b70fbab957b016106653fcca38cd332443b7d977
Shelo <siankhual96@gmail.com>
2019-08-27 19:14:35 +0800

    shelo

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,4 +1,5 @@
 class OrganizationsController < ActionController::Base
+        layout 'application'
     def index
     end
 
--- config/initializers/elasticsearch.rb
+++
@@ -1,2 +0,0 @@
-
-ENV["ELASTICSEARCH_URL"] = "https://vpc-refconnection-ei7hkxysellaeqbzg3efp4xhii.ap-southeast-1.es.amazonaws.com:443"
